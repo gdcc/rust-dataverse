7362dc58589a06a3f47248d2646146c30ee3c5f9
Jan Range <range.jan@web.de>
2024-05-14 20:43:42 +0200

    adapt to structure

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,12 @@ pub mod client;
 pub mod response;
 
 pub mod native_api {
-    pub mod collection;
-    pub mod info;
-}
-
-pub mod models {
-    pub mod collection;
-    pub mod info;
+    pub mod collection {
+        pub mod create;
+    }
+    pub mod info {
+        pub mod version;
+    }
 }
 
 pub mod cli {
