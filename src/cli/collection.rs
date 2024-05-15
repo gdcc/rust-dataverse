e5ab8074de586f0b59efd9ed24e3c2b232ee99f2
Jan Range <range.jan@web.de>
2024-05-15 20:28:45 +0200

    implement collection cli

--- src/cli/collection.rs
+++ src/cli/collection.rs
@@ -1,53 +1,41 @@
+use std::path::PathBuf;
+
 use crate::client::BaseClient;
-use crate::native_api;
-use crate::native_api::collection::create::CollectionCreateBody;
-use clap::{arg, ArgMatches, Command};
+use crate::native_api::collection::create::{self, CollectionCreateBody};
+use crate::native_api::collection::publish;
+use structopt::StructOpt;
 
-use super::base::{get_argument, parse_file};
+use super::base::{evaluate_and_print_response, parse_file, Matcher};
 
-//
-// CLI commands
-//
-pub fn collection_subcommand() -> Command {
-    Command::new("collection")
-        .about("Handle collections in the Dataverse instance")
-        .arg_required_else_help(true)
-        // Subcommands
-        .subcommand(create_subcommand())
+#[derive(StructOpt, Debug)]
+#[structopt(about = "Handle collections of a Dataverse instance")]
+pub enum CollectionSubCommand {
+    Create {
+        #[structopt(long, help = "Alias of the parent dataverse")]
+        parent: String,
+
+        #[structopt(long, help = "Path to the JSON file containing the collection body")]
+        path: PathBuf,
+    },
+    Publish {
+        #[structopt(long, help = "Alias of the collection")]
+        alias: String,
+    },
 }
 
-fn create_subcommand<'a, 'b>() -> Command {
-    Command::new("create")
-        .about("Create a dataverse collection")
-        .arg_required_else_help(true)
-        .args(&[
-            arg!(--parent <NAME> "The parent dataverse alias"),
-            arg!(--file <FILE> "The file containing the collection metadata"),
-        ])
-}
-
-//
-// Execute the appropriate function based on the subcommand
-//
-pub fn collection_matcher(matches: &ArgMatches, client: &BaseClient) {
-    match matches.subcommand() {
-        Some(("create", sub_matches)) => create_collection(sub_matches, client),
-        _ => {
-            println!("No subcommand");
-        }
+impl Matcher for CollectionSubCommand {
+    fn process(&self, client: &BaseClient) {
+        match self {
+            CollectionSubCommand::Create { parent, path } => {
+                let body: CollectionCreateBody =
+                    parse_file::<_, CollectionCreateBody>(path).expect("Failed to parse the file");
+                let response = create::create_collection(client, &parent, &body);
+                evaluate_and_print_response(response);
+            }
+            CollectionSubCommand::Publish { alias } => {
+                let response = publish::publish_collection(client, &alias);
+                evaluate_and_print_response(response);
+            }
+        };
     }
-}
-
-fn create_collection(matches: &ArgMatches, client: &BaseClient) {
-    // Extract the arguments
-    let parent = get_argument::<String, String>(matches, "parent");
-    let path = get_argument::<String, String>(matches, "file");
-    let path = std::path::Path::new(&path);
-
-    // Load the collection metadata from the yaml file
-    let body: CollectionCreateBody =
-        parse_file::<_, CollectionCreateBody>(path).expect("Failed to parse the file");
-
-    let response = native_api::collection::create::create_collection(client, &parent, &body);
-    super::base::evaluate_and_print_response(response);
 }
