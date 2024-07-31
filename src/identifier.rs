8ca06b4289dcc1dbb882da264691999a5891e88b
Jan Range <range.jan@web.de>
2024-07-31 10:51:56 +0200

    add identifier tests

--- src/identifier.rs
+++ src/identifier.rs
@@ -11,7 +11,7 @@ use serde::{Deserialize, Serialize};
 // This way users can supply a general identifier without specifying
 // whether it is a persistent identifier or not. The code will
 // automatically determine the type of identifier and use it.
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(untagged)]
 pub enum Identifier {
     PersistentId(String),
@@ -26,6 +26,42 @@ impl FromStr for Identifier {
         match s.parse::<i64>() {
             Ok(_) => Ok(Identifier::Id(s.parse::<i64>().unwrap())),
             Err(_) => Ok(Identifier::PersistentId(s.to_owned())),
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Tests the conversion of string literals to `Identifier` enum variants.
+    ///
+    /// This test function verifies the `Identifier` enum's ability to correctly parse string literals
+    /// into its variants. It tests with both a numeric string, expected to parse into an `Identifier::Id`,
+    /// and a DOI string, expected to parse into an `Identifier::PersistentId`. The function asserts that
+    /// the parsing process correctly identifies and converts the string literals into the appropriate
+    /// `Identifier` variants.
+    ///
+    /// # Assertions
+    /// - Asserts that a numeric string is correctly parsed as an `Identifier::Id`.
+    /// - Asserts that a DOI string is correctly parsed as an `Identifier::PersistentId`.
+    #[test]
+    fn test_identifier_from_str() {
+        let id = "123";
+        let pid = "doi:10.5072/FK2/ABC123";
+
+        let id = Identifier::from_str(id).unwrap();
+        let pid = Identifier::from_str(pid).unwrap();
+
+        match id {
+            Identifier::Id(id) => assert_eq!(id, 123),
+            _ => panic!("Expected an id"),
+        }
+
+        match pid {
+            Identifier::PersistentId(pid) => assert_eq!(pid, "doi:10.5072/FK2/ABC123"),
+            _ => panic!("Expected a persistent id"),
         }
     }
 }
