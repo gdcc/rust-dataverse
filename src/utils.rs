a1b6cea5bd5bcbf64cc5c6856502c83470acc52a
Jan Range <range.jan@web.de>
2024-07-31 10:53:46 +0200

    add docs

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,21 @@ use crate::{
     client::BaseClient, identifier::Identifier, native_api::dataset::get::get_dataset_meta,
 };
 
-pub async fn get_dataset_id(client: &BaseClient, pid: &Identifier) -> Result<i64, String> {
+/// Retrieves the dataset ID for a dataset identified by a persistent identifier (PID).
+///
+/// This asynchronous function sends a request to retrieve the metadata of a dataset using its PID.
+/// It then extracts and returns the dataset ID from the metadata response.
+///
+/// # Arguments
+///
+/// * `client` - A reference to the `BaseClient` instance used to send the request.
+/// * `pid` - An `Identifier` enum instance representing the unique identifier of the dataset.
+///
+/// # Returns
+///
+/// A `Result` wrapping an `i64` representing the dataset ID if the request is successful,
+/// or a `String` error message on failure.
+pub async fn get_dataset_id(client: &BaseClient, pid: Identifier) -> Result<i64, String> {
     let response = get_dataset_meta(client, pid).await?;
     match response.data {
         Some(data) => Ok(data.id.unwrap()),
